14ac90f73edb7857f104618bfb3bbb25a2a02a51
Joni Sadler <joni.anne.sadler@gmail.com>
2021-01-16 14:49:48 -0500

    Updated Sidebar design

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,30 +2,55 @@ import React from "react";
 import { ReactComponent as Logo } from "../assets/logo.svg";
 import { NavLink } from "react-router-dom";
 import { COLORS } from "../constants";
+import { Home, User, Bell, Bookmark } from "react-feather";
 import styled from "styled-components";
 import GlobalStyles from "../GlobalStyles";
 
 const Sidebar = () => {
   return (
-    <Container>
+    <Wrapper>
       <GlobalStyles />
       <Logo />
-        <h3><NavigationLink to="/" style={{color: "black", textDecoration: "none"}}>Home</NavigationLink></h3>
-        <h3><NavLink to="/:profileId" style={{color: "black", textDecoration: "none"}}>Profile</NavLink></h3>
-        <h3><NavLink to="/notifications" style={{color: "black", textDecoration: "none"}}>Notifications</NavLink></h3>
-        <h3><NavLink to="/bookmarks" style={{color: "black", textDecoration: "none"}}>Bookmarks</NavLink></h3>
-    </Container>
+
+        <SidebarContainer>
+          <Home style={{marginRight: "20px"}}/>
+          <h3><NavigationLink to="/" style={{color: "black", textDecoration: "none"}}>Home</NavigationLink></h3>
+        </SidebarContainer>
+
+        <SidebarContainer>
+          <User style={{marginRight: "20px"}}/>
+          <h3><NavLink to="/:profileId" style={{color: "black", textDecoration: "none"}}>Profile</NavLink></h3>
+        </SidebarContainer>
+
+        <SidebarContainer>
+          <Bell  style={{marginRight: "20px"}}/>
+          <h3><NavLink to="/notifications" style={{color: "black", textDecoration: "none"}}>Notifications</NavLink></h3>
+        </SidebarContainer>
+
+        <SidebarContainer>
+          <Bookmark style={{marginRight: "20px"}}/>
+          <h3><NavLink to="/bookmarks" style={{color: "black", textDecoration: "none"}}>Bookmarks</NavLink></h3>
+        </SidebarContainer>
+
+    </Wrapper>
   );
 };
 
-const Container = styled.div`
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: left;
-  margin-left: 10%;
-  height: 300px;
+  margin: 0% 5% 0% 10%;
+  height: 500px;
 `;
+
+const SidebarContainer = styled.div` 
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    align-items: center;
+`
 
 const NavigationLink = styled(NavLink)` 
   color: blue;
