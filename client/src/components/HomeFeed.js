83e23079485f955fae68b8e77b54c86606f2e853
Joni Sadler <joni.anne.sadler@gmail.com>
2021-01-19 21:38:37 -0500

    Set up tweet feed on homepage

--- client/src/components/HomeFeed.js
+++ client/src/components/HomeFeed.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import SmallTweet from "./SmallTweet";
+import styled from "styled-components";
 
 const HomeFeed = () => {
-  return <div></div>
+  const [tweets, setTweets] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!tweets) {
+        fetch('/api/me/home-feed', {
+            method: "GET",
+          })
+          .then(res => res.json())
+          .then(data => {
+            setTweets(data)
+          })
+    }
+  }, []);
+
+  useEffect(() => {
+    if (tweets) {
+        setLoading(false);
+    }
+  }, [tweets])
+
+//   let {handle} = useParams();
+
+//   useEffect(() => {
+//     fetch(`/api/${handle}/feed`, {
+//       method: "GET",
+//     })
+//     .then(res => res.json())
+//     .then(data => {
+//      console.log("Feed tweets")
+//      console.log(data)
+//     })
+//   }, []);
+
+  console.log(tweets)
+  
+  if (loading) {
+      return (
+          <div>Loading</div>
+      )
+  }
+
+  return (
+  <Wrapper>
+    
+    {tweets.tweetIds.map((tweetId) => {
+        const tweet = tweets.tweetsById[tweetId]
+        return (
+            <SmallTweet
+ 
+                tweet={tweet}
+            />
+        )
+    })}
+  </Wrapper>
+  )
 };
+
+const Wrapper = styled.div` 
+  width: 700px;
+`;
 
 export default HomeFeed;
--- client/src/components/SmallTweet.js
+++ client/src/components/SmallTweet.js
@@ -1,19 +1,71 @@
-import React, { useContext } from "react";
+import React from "react";
 import TweetActions from "./TweetActions";
-import { CurrentUserContext } from "./CurrentUserContext"
+import styled from "styled-components";
 
-const SmallTweet = () => {
-  const {currentUser, status} = useContext(CurrentUserContext);
+const SmallTweet = ({tweet}) => {
+  console.log(tweet);
+
+               // author={tweetsById.author}
+                // isLiked={tweetsById.isLiked}
+                // isRetweeted={tweetsById.isRetweeted}
+                // media={tweetsById.media}
+                // numLikes={tweetsById.numLikes}
+                // numRetweets={tweetsById.numRetweets}
+                // tweetText={tweetsById.status}
+                // timestamp={tweetsById.timestamp}
+
 
   return (
-  <div>
-    <Avatar />
-    <Username>{currentUser}</Username>
-    <Handle></Handle>
-    <TweetContent></TweetContent>
-    <TweetActions />
-  </div>
+    <Wrapper>
+      <TweetHeader>
+        <img src={tweet.author.avatarSrc} style={{borderRadius: "50%"}} height="60px" width="60px" />
+        <DisplayName>{tweet.author.displayName}</DisplayName>
+        <Handle>@{tweet.author.handle} •</Handle>
+        <Timestamp>{tweet.timestamp}</Timestamp>
+      </TweetHeader>  
+      <TweetActionWrapper>
+          <TweetActions />
+      </TweetActionWrapper>
+    </Wrapper>
   )
-}
+};
+
+const Wrapper = styled.div` 
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 40px;
+  border: 1px solid black;
+  height: 300px;
+  padding: 10px;
+`;
+
+const TweetHeader = styled.div` 
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const DisplayName = styled.p` 
+  font-size: 16px;
+  font-weight: 600;
+  padding-left: 10px;
+`;
+
+const Handle = styled.p` 
+  font-size: 14px;
+  color: gray;
+  padding: 4px 0px 0px 5px;
+`;
+
+const Timestamp = styled.p` 
+  font-size: 14px;
+  color: gray;
+  padding: 4px 0px 0px 5px;
+`
+
+const TweetActionWrapper = styled.div` 
+  justify-content: space-evenly;
+`;
+
 
 export default SmallTweet;
